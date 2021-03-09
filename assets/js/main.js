6194e5012fb2d7509dd54d16860bc726791d75d5
Justinator <32310311+Justinator@users.noreply.github.com>
2021-03-09 00:30:40 -0600

    updated starter theme with additions from recent projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,15 +7,58 @@ import { pageTransitionOut, pageTransitionIn, contentAnimation, updateMenu } fro
 barba.use(barbaPrefetch);
 gsap.registerPlugin(ScrollTrigger);
 
-const menuButton = document.querySelector(".menu-button-wrap");
 const menu = document.querySelector(".nav-list");
 const hamburger = document.querySelector(".hamburger");
+const navClasses = document.querySelector('.site-header');
+let scrollState = 0;
 
-menuButton.addEventListener("click", toggleMobileMenu);
+var scrollTop = function() {
+  return window.scrollY;
+};
+
+var scrollDetect = function(collapse, expand) {
+  var currentScroll = scrollTop();
+  if (currentScroll > scrollState) {
+    collapse();
+  } else {
+    expand();
+  }
+  scrollState = scrollTop();
+};
+
+function collapseNav() {
+  navClasses.classList.remove('expand');
+  navClasses.classList.add('collapse');
+}
+
+function expandNav() {
+  navClasses.classList.remove('collapse');
+  navClasses.classList.add('expand');
+}
+
+window.addEventListener("scroll", function() {
+  scrollDetect(collapseNav, expandNav);
+});
+
+hamburger.addEventListener("click", toggleMobileMenu);
 
 function toggleMobileMenu() {
-    menu.classList.toggle("nav-open");
-    hamburger.classList.toggle("is-active");
+    if(menu.classList.contains("nav-open")) {
+        this.setAttribute("aria-expanded", "false");
+        this.setAttribute("aria-label", "open mobile menu");
+        menu.classList.remove("nav-open");
+        hamburger.classList.remove("is-active");
+    } else {
+        menu.classList.add("nav-open");
+        hamburger.classList.add("is-active");
+        this.setAttribute("aria-expanded","true");
+        this.setAttribute("aria-label","close mobile menu");
+    }
+}
+
+function updateAria() {
+    hamburger.setAttribute("aria-expanded", "false");
+    hamburger.setAttribute("aria-label", "open mobile menu");
 }
 
 const selectAll = (e) => document.querySelectorAll(e);
@@ -26,19 +69,6 @@ function fadeInContent() {
     const fadeUp = document.querySelectorAll(".fade-up");
     const blog = document.querySelector(".blog-container");
 
-    if (document.body.contains(introSection)) {
-        gsap.from(introSection, {
-            opacity: 0,
-            y: 20,
-            duration: .5,
-            ease: 'Power2.in',
-            scrollTrigger: {
-                trigger: introSection,
-                start: "top bottom-=50",
-                toggleActions: "play none none reset",
-            }
-        });
-    }
     gsap.utils.toArray(fadeUp).forEach((fade) => {
         gsap.from(fade, {
             opacity: 0,
@@ -95,6 +125,7 @@ function initPageTransitions() {
     // do something after the transition finishes
     barba.hooks.after(() => {
         homepageAnimations();
+        updateAria();
         ga('set', 'page', window.location.pathname);
         ga('send', 'pageview');
     });
--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,21 @@
+const autoprefixer = require('autoprefixer')
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
-    plugins: [
-        require('autoprefixer')()
-    ]
-};
+  plugins: [
+    autoprefixer(),
+    // purgecss({
+    //   content: [
+    //     './layouts/**/*.html',
+    //     './layouts/**/*.svg',
+    //     './content/**/*.md',
+    //   ],
+    //   safelist: [
+    //     'is-active',
+    //     'nav-open',
+    //     'collapse',
+    //     'expand',
+    //   ],
+    // }),
+  ],
+}
